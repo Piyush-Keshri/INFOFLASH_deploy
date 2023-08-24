37c05e401381f1c36811b78daa1b30c48e5d8284
Piyush Keshri <piyushkeshri383@gmail.com>
2023-08-24 16:56:25 +0530

    Responsive

--- client/src/components/Articles.jsx
+++ client/src/components/Articles.jsx
@@ -1,37 +1,41 @@
+// Import necessary components and libraries
 import { Box } from "@mui/material";
 import { useEffect, useState } from "react";
-import { getNews } from "../service/api";
+import { getNews } from "../service/api"; // Import a function to fetch news data
 
-import Article from "./Article";
+import Article from "./Article"; // Import the Article component
 
+// Create a functional component named Articles
 const Articles = () => {
 
-
+    // Define a useEffect hook to run when the component mounts
     useEffect(() => {
+        // Call the dailyNews function
         dailyNews();
     }, []);
 
-
+    // Define a state variable 'news' using the useState hook, initially an empty array
     const [news, setNews] = useState([]);
+
+    // Define a function 'dailyNews' to fetch news data
     const dailyNews = async () => {
+        // Call the 'getNews' function to fetch news data and set it in the 'news' state
         let response = await getNews();
         setNews(response.data);
     }
 
-
-
+    // Render the component
     return (
-
         <Box>
+            {/* Map over the 'news' array and render an 'Article' component for each data item */}
             {
                 news.map(data => (
                     <Article data={data} />
                 ))
             }
         </Box>
-
     )
-
 }
 
+// Export the 'Articles' component as the default export
 export default Articles;
--- client/src/components/InfoHeader.jsx
+++ client/src/components/InfoHeader.jsx
@@ -1,42 +1,89 @@
-import { Box, Typography, styled } from '@mui/material'
+// import { Box, Typography, styled } from '@mui/material'
 
+// const StyledBox = styled(Box)`
+//     background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+//     background-size: 400% 400%;
+//     animation: gradient 15s ease infinite;
+//     margin: 10px 70px;
+//     height: 10vh;
+//     border-radius: 10px;
+
+//     @keyframes gradient {
+//         0% {
+//             background-position: 0% 50%;
+//         }
+//         50% {
+//             background-position: 100% 50%;
+//         }
+//         100% {
+//             background-position: 0% 50%;
+//         }
+//     }
+
+//     /* Media query to hide the component below 900px screen width */
+//     @media (max-width: 900px) {
+//         display: none;
+//     }
+// `
+
+// const StyledText = styled(Typography)`
+//     color: beige;
+//     font-size: 22px;
+//     padding: 8px;
+//     text-align: center;
+// `
+
+// const InfoHeader = () => {
+//     return (
+//         <StyledBox>
+//             <StyledText>From local stories to global events, stay informed with our comprehensive news coverage.</StyledText>
+//         </StyledBox>
+//     )
+// }
+
+// export default InfoHeader;
+
+import { Box, Typography, styled } from '@mui/material'
 
 const StyledBox = styled(Box)`
     background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
-	background-size: 400% 400%;
-	animation: gradient 15s ease infinite;
-    margin: 10px 70px;
-    height:10vh;
-    border-radius:10px;
+    background-size: 400% 400%;
+    animation: gradient 15s ease infinite;
+    margin: 10px 10px; /* Adjust margin for mobile */
+    padding: 10px; /* Add some padding for mobile */
+    border-radius: 10px;
+    text-align: center; /* Center text for mobile */
 
-@keyframes gradient {
-	0% {
-		background-position: 0% 50%;
-	}
-	50% {
-		background-position: 100% 50%;
-	}
-	100% {
-		background-position: 0% 50%;
-	}
-}  
+    @keyframes gradient {
+        0% {
+            background-position: 0% 50%;
+        }
+        50% {
+            background-position: 100% 50%;
+        }
+        100% {
+            background-position: 0% 50%;
+        }
+    }
+
+    /* Media query for mobile devices */
+    @media (max-width: 600px) {
+        margin: 10px; /* Adjust margin for smaller screens */
+        height: auto; /* Let the height be determined by content */
+    }
 `
+
 const StyledText = styled(Typography)`
-    color:beige;
-    font-size : 22px;
-    padding:8px;
-    text-align:center;
+    color: beige;
+    font-size: 16px; /* Adjust font size for mobile */
 `
 
 const InfoHeader = () => {
-
     return (
         <StyledBox>
             <StyledText>From local stories to global events, stay informed with our comprehensive news coverage.</StyledText>
         </StyledBox>
-
     )
-
 }
 
 export default InfoHeader;
